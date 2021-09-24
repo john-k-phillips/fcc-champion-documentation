d9199044ed48e0141ffb59df89efb8dfba0ec397
John Phillips <johnphillips.dev@gmail.com>
2021-09-24 10:57:11 +0100

    Added filter section and changed list with anchor.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let langaugeType = 'en_US';
 
+// ELEMENTS
+const navList = document.getElementById('#championList');
+
 fetch('https://ddragon.leagueoflegends.com/cdn/languages.json')
     .then(response => response.json())
     .then((data) => {
@@ -18,9 +21,13 @@ fetch('http://ddragon.leagueoflegends.com/cdn/11.19.1/data/en_US/champion.json')
 
 // FUNCTION TO CREATE CHAMPION ITEMS ON NAVIGATION LIST
 const createListItem = (champion) => {
-    const navList = document.getElementById('#championList');
     const listItem = document.createElement('li');
-    listItem.textContent = champion;
+    const a = document.createElement('a');
+
+    a.href = `#${champion}`
+    a.textContent = champion
+
+    listItem.appendChild(a);
     navList.appendChild(listItem);
 }
 
@@ -34,6 +41,25 @@ const expandNav = (event) => {
     } else {
         navSection.style.display = 'flex';
         mainSection.style.display = 'none';
+    }
+}
+
+const filterSearch = () => {
+    input = document.querySelector('#input');
+    console.log(input);
+    filter = input.value.toUpperCase();
+    li = navList.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0];
+
+        txtValue = a.textContent || a.innerText;
+
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
     }
 }
 
